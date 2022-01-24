f4d6706a54af096b84a55c38f843ef5c5c4a13df
LauraSilvaG <laugrand@gmail.com>
2022-01-24 18:51:36 +0100

    ejemplos

--- js/rutas.js
+++ js/rutas.js
@@ -50,3 +50,19 @@ function zoomToRutas(valores) {
 
     } //fin funcion
 
+    var animacion;
+    function rotarCamara(timestamp) {
+
+        rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+        map.rotateTo(rotacion, { duration: 0 });
+
+        animacion = requestAnimationFrame(rotarCamara);
+    }
+
+
+    function finalRotarCamara() {
+
+        cancelAnimationFrame(animacion);
+    }   
+
+
