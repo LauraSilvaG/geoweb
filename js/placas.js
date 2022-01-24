e9f6871dd8e5b979aa177bc8adc438990dd381ac
LauraSilvaG <laugrand@gmail.com>
2022-01-24 21:02:04 +0100

    terremotos

---
+++ js/placas.js
@@ -0,0 +1,35 @@
+function addPlacas() {
+
+    var url = 'datos/placas.geojson';
+    map.addSource('placas', { type: 'geojson', data: url});
+
+         map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': 'placas',
+            'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+            },
+            'paint': {
+            'line-color': '#ff0000',
+            'line-width': 3
+            }
+    });
+
+} //fin funcion
+
+
+function zoomToRutas(valores) {
+
+    var coord = valores.split("/");
+
+    map.flyTo({
+            center: [coord[2], coord[1]],
+            zoom: coord[0]
+        });
+
+    } //fin funcion
+
+
+
